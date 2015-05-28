b194e6cef1ca09ed68385e5906c40bf77be65720
david karapetyan <root@davidk-linux.yojoe.local>
2015-05-28 13:06:55 -0700

    rotate and symlink

--- bin/keyrotate.rb
+++ bin/keyrotate.rb
@@ -1,7 +1,11 @@
 require 'openssl'
 require 'pathname'
+require 'fileutils'
 
-last_key = Dir['keys/*'].select {|f| Pathname.new(f).basename.to_i}.sort.reverse.first
+last_key = Dir['keys/*'].map {|f| Pathname.new(f).basename.to_s.to_i}.sort.reverse.first
 cipher = OpenSSL::Cipher.new('aes-256-gcm')
 new_key = cipher.random_key
-open("keys/#{last_key + 1}", 'w') {|f| f.write(new_key)}
+new_key_number = last_key + 1
+key_path = "keys/#{new_key_number}"
+open(key_path, 'w') {|f| f.write(new_key)}
+FileUtils.ln_s(new_key_number.to_s, "keys/latest", :force => true)
