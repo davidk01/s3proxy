f19fce698214421151fba005f85936a37373d59f
david karapetyan <root@davidk-linux.yojoe.local>
2015-05-29 11:01:52 -0700

    expiration helper script

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source "https://rubygems.org"
 
 gem 'thin'
 gem 'sinatra'
+gem 'trollop'
 
 group :development do
   gem 'pry'
--- bin/expire.rb
+++ bin/expire.rb
@@ -1,1 +1,24 @@
-# TODO: list of file paths relative to uploads that should be expired
+require 'bundler/setup'
+require 'trollop'
+require 'pathname'
+require_relative '../lib/constants'
+
+opts = Trollop::options do
+  opt :file, "Locations of files relative to #{UPLOADS} directory",
+    :required => true, :type => :strings, :multi => false
+end
+
+# Validate
+paths = opts[:file].map {|f| Pathname.new(File.join(UPLOADS, f)).cleanpath}
+paths.each do |p|
+  raise StandardError, "File does not exist #{p}." unless p.exist?
+  raise StandardError, "Can not expire symlinks #{p}." if p.symlink?
+end
+
+# Create the directory structure in TOENCRYPT and add the symlinks
+opts[:file].each do |f|
+  symlink_source = Pathname.new(File.join(TOENCRYPT, f)).cleanpath
+  FileUtils.mkdir_p(p) unless p.dirname.exist?
+  symlink_target = Pathname.new(File.join(File.expand_path(File.dirname __FILE__), UPLOADS, f)).cleanpath
+  FileUtils.ln_s(symlink_target, p)
+end
